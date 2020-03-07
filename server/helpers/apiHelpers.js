6c10354d33783da981209f6508b5e247fc8aa1b3
Joe Yan <chengmu.yan@gmail.com>
2020-03-06 19:57:41 -0500

    Complete getGenres in apiHelpers

--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -2,11 +2,16 @@ const request = require("request");
 const axios = require("axios");
 const { API_KEY } = require("../../config.js");
 
+const TMDB_URL = "https://api.themoviedb.org/3";
+
 // write out logic/functions required to query TheMovieDB.org
+module.exports.getGenres = () => {
+  let config = { params: { api_key: API_KEY } };
+  return axios.get(TMDB_URL + "/genre/movie/list", config).then(({ data }) => data);
+};
 
 // FOR REFERENCE:
 // https://www.themoviedb.org/account/signup
 // https://developers.themoviedb.org/3/discover/movie-discover
 // Get your API Key and save it in your config file
-
 // Don't forget to export your functions and require them within your server file
