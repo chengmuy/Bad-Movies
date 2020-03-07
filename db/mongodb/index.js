53da196d5f5f707c0ae4a5977962383e887e1565
Joe Yan <chengmu.yan@gmail.com>
2020-03-07 10:24:31 -0500

    Set up mongo schema and Movie model

--- db/mongodb/index.js
+++ db/mongodb/index.js
@@ -1,18 +1,26 @@
-// 
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
 
-const mongoose = require('mongoose');
-if(process.env.MONGODB_URI){
-  mongoose.connect(process.env.MONGODB_URI)
-} else{
-  mongoose.connect('mongodb://localhost:27017/badmovies', { useNewUrlParser: true });
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect("mongodb://localhost:27017/badmovies", { useNewUrlParser: true });
 }
 
 const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("Connected to db...");
+});
 
-mongoose.Promise = Promise;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-  console.log('Connected to db...');
-})
+const movieSchema = new mongoose.Schema({
+  poster_path: String,
+  title: String,
+  release_date: String,
+  vote_average: Number
+});
 
-module.exports.db = db
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports.db = db;
+module.exports.Movie = Movie;
