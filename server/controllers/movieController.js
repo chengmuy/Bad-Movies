74d284e64142436f1a76b13379e89251bc6fcede
Joe Yan <chengmu.yan@gmail.com>
2020-03-07 09:47:29 -0500

    Set up getGenres and getSearch routes

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,20 +1,25 @@
 const movieModel = require("../models/movieModel.js");
-const apiHelpers = require("../helpers/apiHelpers.js");
+const tmdbApi = require("../helpers/apiHelpers.js");
 
 //Return requests to the client
 module.exports = {
   getGenres: (req, res) => {
-    // make an axios request to get the list of official genres
-    // use this endpoint, which will also require your API key: https://api.themoviedb.org/3/genre/movie/list
-    // send back
+    tmdbApi
+      .getGenres()
+      .then(data => res.json(data))
+      .catch(err => {
+        console.error(err);
+        res.sendStatus(500);
+      });
   },
   getSearch: (req, res) => {
-    // get the search genre
-    // https://www.themoviedb.org/account/signup
-    // get your API KEY
-    // use this endpoint to search for movies by genres, you will need an API key
-    // https://api.themoviedb.org/3/discover/movie
-    // and sort them by horrible votes using the search parameters in the API
+    tmdbApi
+      .getSearch(req.body)
+      .then(data => res.json(data))
+      .catch(err => {
+        console.error(err);
+        res.sendStatus(500);
+      });
   },
 
   saveMovie: (req, res) => {
